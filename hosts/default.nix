1c6de47bcbb297cd9d0d8fd1a94a0d8842e726a3
kadoppe <kadoppe@me.com>
2024-10-13 22:16:51 +0900

    add hosts

---
+++ hosts/default.nix
@@ -0,0 +1,75 @@
+inputs:
+let
+  mkNixosSystem =
+    {
+      system,
+      hostname,
+      username,
+      modules,
+    }:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system modules;
+      specialArgs = {
+        inherit inputs hostname username;
+      };
+    };
+
+  mkHomeManagerConfiguration =
+    {
+      system,
+      username,
+      overlays,
+      modules,
+    }:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        inherit system overlays;
+        config = {
+          allowUnfree = true;
+
+          # FIX: How to solve this?
+          permittedInsecurePackages = [ "electron-25.9.0" ];
+        };
+      };
+      extraSpecialArgs = {
+        inherit inputs username;
+        theme = (import ../themes) "tokyonight-moon";
+        pkgs-stable = import inputs.nixpkgs-stable {
+          inherit system overlays;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      };
+      modules = modules ++ [
+        {
+          home = {
+            inherit username;
+            homeDirectory = "/home/${username}";
+            stateVersion = "22.11";
+          };
+          programs.home-manager.enable = true;
+          programs.git.enable = true;
+        }
+      ];
+    };
+in
+{
+  nixos = {
+    terra = mkNixosSystem {
+      system = "aarch64-linux";
+      hostname = "moca";
+      username = "kadoppe";
+      modules = [ ./moca/nixos.nix ];
+    };
+  };
+
+  home-manager = {
+    "kadoppe@moca" = mkHomeManagerConfiguration {
+      system = "aarch64-linux";
+      username = "kadoppe";
+      overlays = [ inputs.fenix.overlays.default ];
+      modules = [ ./moca/home-manager.nix ];
+    };
+  };
+}
---
+++ hosts/moca/nixos.nix
@@ -0,0 +1,61 @@
+{
+  inputs,
+  pkgs,
+  username,
+  ...
+}:
+{
+  imports =
+    [
+      # ./hardware-configuration.nix
+
+      # ../../modules/core
+      # ../../modules/desktop
+      # ../../modules/programs/flatpak.nix
+      # ../../modules/programs/hyprland.nix
+      # ../../modules/programs/media.nix
+      # ../../modules/programs/nix-ld.nix
+      # ../../modules/programs/secureboot.nix
+      # ../../modules/programs/shell.nix
+      # ../../modules/programs/steam.nix
+    ]
+    # ++ (with inputs.nixos-hardware.nixosModules; [
+    #   common-cpu-amd
+    #   common-gpu-amd
+    #   common-pc-ssd
+    # ]);
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  };
+
+  # Don't touch this
+  system.stateVersion = "22.11";
+
+  users.users."${username}" = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "audio"
+      "video"
+    ];
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = ''
+          ${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland
+        '';
+        user = username;
+      };
+    };
+  };
+}
