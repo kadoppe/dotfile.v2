0197cd004267934fd3b5c29fba70e9a83acee07a
kadoppe <kadoppe@me.com>
2024-10-13 23:31:07 +0900

    kernel

--- hosts/moca/nixos.nix
+++ hosts/moca/nixos.nix
@@ -30,7 +30,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    # kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = prl-tools;
   };
 
   # Don't touch this
